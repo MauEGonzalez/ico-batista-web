cb1de769506747cc9631a9d669e676b5c3330d57
Mauro Gonzalez <maurogonzalez97@hotmail.com>
2025-11-13 12:36:11 -0300

    Fix: responsive header y build /tienda page

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,16 +1,33 @@
+// /src/pages/ShopPage/ShopPage.jsx
+
 import React from 'react';
 import Header from '../../components/layout/Header/Header.jsx';
 import Footer from '../../components/layout/Footer/Footer.jsx';
+import { Link } from 'react-router-dom';
 import styles from './ShopPage.module.css';
 
 const ShopPage = () => {
   return (
     <div>
       <Header />
-      <main className={styles.mainContent}>
-        <h1>Tienda</h1>
-        <p>Las colecciones de la tienda se mostrarán aquí.</p>
-      </main>
+      {/* Usamos un div en lugar de <main> para tener control total del layout */}
+      <div className={styles.shopContainer}>
+        
+        {/* Panel de Hombre */}
+        <Link to="/hombre/formal" className={styles.portalLink}>
+          <div className={`${styles.portalPanel} ${styles.hombre}`}>
+            <h2 className={styles.portalTitle}>Moda Hombre</h2>
+          </div>
+        </Link>
+        
+        {/* Panel de Mujer */}
+        <Link to="/mujer/formal" className={styles.portalLink}>
+          <div className={`${styles.portalPanel} ${styles.mujer}`}>
+            <h2 className={styles.portalTitle}>Moda Mujer</h2>
+          </div>
+        </Link>
+
+      </div>
       <Footer />
     </div>
   );
